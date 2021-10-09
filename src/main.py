6249b502dba1f78d7af00bfcad8090c4024d86d6
Andrew Frauens <AndrewFrauens@users.noreply.github.com>
2021-10-09 03:01:03 -0500

    removed auto-generated header information from main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import cv2 as cv2
 import numpy as np
 
@@ -56,5 +51,3 @@ if __name__ == '__main__':
 
     vid.release()
     cv2.destroyAllWindows()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
